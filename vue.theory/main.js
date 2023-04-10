9c31888f80c64fd52e50c6020765ec73e7d7601b
BigmacKiller <varik_varibrus@mail.ru>
2023-04-11 00:12:23 +0300

    8. cumputed properties
    and added solving

--- vue.theory/main.js
+++ vue.theory/main.js
@@ -5,22 +5,17 @@ createApp({
     data() {
         return {
             cart: 0,
+            brand: 'Vue mastery',
             product: 'Socks',
+            sale: 'is on sale',
             description: 'These socks are soft and smell nice',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: 'https://vk.com/variar',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
               ],
-            sizes: [
-                {id: 1131, size: 's'},
-                {id: 1132, size: 'm'},
-                {id: 1133, size: 'l'},
-                {id: 1134, size: 'xl'},
-            ]
         }
     },
     methods: {
@@ -32,8 +27,22 @@ createApp({
                 this.cart -=1
             }
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product + ' ' + this.onSale
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].quantity ? this.sale: ''
         }
     }
 }).mount('#app')
