6e6907304c127f0bb927d9d0f334581b3d28f047
BigmacKiller <varik_varibrus@mail.ru>
2023-04-12 17:36:06 +0300

    I change this code. becouse it doesn't worked, but now it work

---
+++ vue.theory/components/ProductDisplay.js
@@ -0,0 +1,96 @@
+app.component(
+  // the registered name
+  'product-display',
+  // the implementation
+  {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<div class="product-display">
+            <div class="product-container">
+                <div class="product-image">
+                    <img :class="{ 'out-of-stock-img': !inStock }" :src="image" alt="" srcset="">
+                </div>
+                <div class="product-info">
+                    <h1>{{ title }}</h1>
+                    <p v-if="inStock">In Stock</p>
+                    <p v-else>Out in Stock</p>
+                    <p>Shipping: {{ shipping }}</p>
+                    <ul>
+                        <li v-for="detail in details">{{ detail }}</li>
+                    </ul>   
+                    <div 
+                    v-for="(variant, index) in variants" 
+                    :key="variant.id" 
+                    @mouseover="updateVariant(index)"
+                    class="color-circle" 
+                    :style="{ backgroundColor: variant.color }"
+                    ></div>
+                    <!-- <div v-for="size in sizes">{{ size.size }}</div> -->
+                    <button class="button" :disabled="!inStock" :class="{ disabledButton: !inStock }" @click="addToCard">Add to Cart</button>
+                    <button class="button" @click="removeToCard">Remove item</button>
+                </div>
+            </div>
+        </div>`,
+        data() {
+            return {
+                brand: 'Vue mastery',
+                product: 'Socks',
+                sale: 'is on sale',
+                description: 'These socks are soft and smell nice',
+                selectedVariant: 0,
+                url: 'https://vk.com/variar',
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants: [
+                    { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                    { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+                  ],
+                // onSale: true
+            }
+        },
+        methods: {
+            addToCard(){
+                this.cart += 1
+            },
+            removeToCard(){
+                if(this.cart !== 0) {
+                    this.cart -=1
+                }
+            },
+            updateVariant(index){
+                this.selectedVariant = index
+            }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product + ' ' + this.onSale
+            },
+            image(){
+                return this.variants[this.selectedVariant].image
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].quantity
+            },
+            onSale() {
+                return this.variants[this.selectedVariant].quantity ? this.sale: ''
+            },
+            shipping(){
+                if (this.premium) {
+                    return 'Free'
+                }
+                return 2.99
+            }
+            // sale() {
+            //     if (this.onSale) {
+            //         return this.brand + ' ' + this.product + ' is on sale.'
+            //     }
+            //     return ''
+            // }
+        }
+  }
+)
--- vue.theory/main.js
+++ vue.theory/main.js
@@ -1,48 +1,9 @@
-
-const { createApp } = Vue
-    
-createApp({
+const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            brand: 'Vue mastery',
-            product: 'Socks',
-            sale: 'is on sale',
-            description: 'These socks are soft and smell nice',
-            selectedVariant: 0,
-            url: 'https://vk.com/variar',
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-              ],
+            premium: true
         }
     },
-    methods: {
-        addToCard(){
-            this.cart += 1
-        },
-        removeToCard(){
-            if(this.cart !== 0) {
-                this.cart -=1
-            }
-        },
-        updateVariant(index){
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product + ' ' + this.onSale
-        },
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        onSale() {
-            return this.variants[this.selectedVariant].quantity ? this.sale: ''
-        }
-    }
-}).mount('#app')
+    methods: {}
+})
