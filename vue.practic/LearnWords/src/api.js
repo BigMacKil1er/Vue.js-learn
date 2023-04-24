01a0223984ad48b764679d69ab23aeeca93e0caf
BigmacKiller <varik_varibrus@mail.ru>
2023-04-24 21:11:07 +0300

    before change

--- vue.practic/LearnWords/src/api.js
+++ vue.practic/LearnWords/src/api.js
@@ -3,13 +3,29 @@ const tickersHandlers = new Map()
 const AGGREGATE_INDEX = '5'
 
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
+let worker = new SharedWorker('src/worker.js')
 
 socket.addEventListener('message', a => {
-    const { TYPE: type, FROMSYMBOL: currancy, PRICE: newPrice} = JSON.parse(a.data)
+    const { TYPE: type, FROMSYMBOL: currancy, PRICE: newPrice, PARAMETER: param} = JSON.parse(a.data)
+    // if (param) {
+    //     const tikers = param.split('~')
+    //     if (type === '500' && tikers[3] !== 'BTC') {
+    //         console.log(tikers[2]);
+    //         subscribeToWebSocketBTC(tikers[2])
+    //     } else if(tikers[3] === 'BTC') {
+    //         return
+    //         // console.log('this is Btc');
+    //     }
+    // }
+    const handlers = tickersHandlers.get(currancy) ?? []
+    // worker.port.postMessage([currancy, newPrice])
+    // worker.port.onmessage = function(e){
+    //             console.log(e.data);
+    //         }
     if (type !== AGGREGATE_INDEX || newPrice === undefined) {
         return
     }
-    const handlers = tickersHandlers.get(currancy) ?? []
+    
     handlers.forEach(fn => fn(newPrice))
 })
 
@@ -24,6 +40,14 @@ function sendToWebScket(message){
         socket.send(stringifyMessage)
     }, { once: true })
 }
+
+function subscribeToWebSocketBTC(ticker){
+    sendToWebScket({
+        "action": "SubAdd",
+        "subs": [`5~CCCAGG~${ticker}~BTC`]  
+    })
+}
+
 function subscribeToWebSocket(ticker){
     sendToWebScket({
         "action": "SubAdd",
@@ -38,12 +62,17 @@ function unsubscribeFromWebSocket(ticker){
 }
 
 export const subscribeToTicker = (ticker, callback) => {
-    const subscribers = tickersHandlers.get(ticker) || []
-    tickersHandlers.set(ticker, [...subscribers, callback])
-    subscribeToWebSocket(ticker)
+    worker.port.postMessage([ticker])
+    worker.port.onmessage = function(e){
+                    callback(e.data);
+                }
+    fn => fn(newPrice)
+    // const subscribers = tickersHandlers.get(ticker) || []
+    // tickersHandlers.set(ticker, [...subscribers, callback])
+    // subscribeToWebSocket(ticker)
 }
 // export const unsubscribeFromTicker = (ticker, callback) => {
-//     const subscribers = tickersHandlers.get(ticker) || []
+//     const subscribers = tickersHandlers.get(ticker) || []    
 //     tickersHandlers.set(ticker, subscribers.filter(fn = fn !== callback))
 // }
 export const unsubscribeFromTicker = (ticker) => {
@@ -51,4 +80,12 @@ export const unsubscribeFromTicker = (ticker) => {
     unsubscribeFromWebSocket(ticker)
 }
 
+// function sendToWorker(ticker){
+//     console.log(ticker);
+//     worker.port.postMessage([ticker])
+//     console.log('sended');
+//     worker.port.onmessage = function(e){
+//         console.log(e.data);
+//     }
+// }
 window.tickers = tickersHandlers
